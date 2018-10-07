b61164ac1e2a28bfdb29d4d7abaed6b0a2218e3d
asd346119 <asd346119@yahoo.com.tw>
2018-10-07 22:05:05 +0800

    hw2

---
+++ hw2-20/src/h20.java
@@ -0,0 +1,12 @@
+
+public class h20 {
+public static void main (String[] args )
+{
+	int[] test = {80,60,22,50,75};
+	for(int i=0;i<test.length; i++)
+	{
+		System.out.println("第" +(+1)+ "個人的分數是" +test[i]+ "分");
+	}		
+System.out.println("考試的人數為"+ test.length + "人");
+}
+}
